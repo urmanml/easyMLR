9b6b4532e3085d5158b1630e214c728c715c2e18
urmanml <48004810+urmanml@users.noreply.github.com>
2019-03-04 18:31:29 +0530

    Delete featureNormalization.R

--- R/R/featureNormalization.R
+++
@@ -1,8 +0,0 @@
-#' normalize features
-featureNormalization<-function(dataset,target)
-{
-dataset<-normalizeFeatures(dataset, target = target, method = "range",
-                           #cols = NULL,
-                           range = c(0, 1), on.constant = "quiet")
-return(dataset)
-}
