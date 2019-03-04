ae6e73944bb44196edd195f5b93f2b8897278c12
urmanml <48004810+urmanml@users.noreply.github.com>
2019-03-04 18:31:56 +0530

    Delete outlierTreatment.R

--- R/R/outlierTreatment.R
+++
@@ -1,7 +0,0 @@
-#' outlier treatment function
-outlierTreatment<-function(dataset,target)
-{
-  #currently not capping any values, additional arguments need to be passed
-  dataset<-capLargeValues(dataset,target = target)
-  return(dataset)
-}
